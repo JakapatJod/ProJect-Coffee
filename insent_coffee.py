a26f54b3437fc5055a9b12d9a4d9e2e78448038a
5656ar <nuat123456@gmail.com>
2023-03-20 18:34:50 +0700

    up

--- insent_coffee.py
+++ insent_coffee.py
@@ -3,11 +3,11 @@ import random
 import time
 
 x = random.randint(100,1000)
-billnumber = 'Cake'
+billnumber = ['Espresso','Cappucino','Latte','Mocha','Americano','Macchiato','Green Tea','Black Tea','Chocolate','White Chocolate','Milk','Thai Tea','Cupcake','Dounut','Cheesecake','Pudding','Waffle,','Cake']
 
-sub = 80
+sub = [55,40,45,50,20,50,30,40,30,40,30,35,55,30,75,80,65,70]
 
-date = '65'
+date = '100'
 
 
 con = pymysql.connect(host = "localhost" ,user = "root" ,password = "Bunnapon122")
@@ -16,7 +16,10 @@ mycursor = con.cursor()
 query = 'use userdata'
 mycursor.execute(query)
 
-query = 'insert into stock_main(coffee_name ,quantity , price) values(%s,%s,%s)'
-mycursor.execute(query,(billnumber,date,sub))
+for i in range(0,18):
+    # print(billnumber[i])
+    # print(sub[i])
+    query = 'insert into stock_main(coffee_name ,quantity , price) values(%s,%s,%s)'
+    mycursor.execute(query,(billnumber[i],date,sub[i]))
 con.commit()
 con.close() 
